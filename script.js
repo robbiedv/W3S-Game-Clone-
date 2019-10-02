e0c1ee21ecafd420354c8b818604406998200062
robbiedv <robbie.c.devries@gmail.com>
2019-10-01 20:59:30 -0500

    game piece now displays, WASD control movement

--- script.js
+++ script.js
@@ -2,26 +2,63 @@
 var myGamePiece;
 
 function startGame() {
+  myGamePiece = new component(30, 30, "blue", 10, 120);
   myGameArea.start();
-  myGamePiece = new component (30,30, "red", 10, 120);
+}
+
+var myGameArea = {
+  canvas : document.createElement("canvas"),
+  start : function() {
+    this.canvas.width = 480;
+    this.canvas.height = 270;
+    this.context = this.canvas.getContext("2d");
+    document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    this.interval = setInterval(updateGameArea, 20);
+    window.addEventListener("keydown", function(e) {
+      myGameArea.key = e.keyCode;
+    });
+    window.addEventListener("keyup", function(e) {
+      myGameArea.key = false;
+    });
+
+  },
+  clear : function() {
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  }
 }
 
 function component(width, height, color, x, y) {
   this.width = width;
   this.height = height;
   this.x = x;
-  this.y
-  ctx = myGameArea.context;
-  ctx.fillStyle = color;
-  ctx.fillRect(this.y, this.x, this.width, this.height);
+  this.y = y;
+  this.speedX = 0;
+  this.speedY = 0;
+  this.update = function() {
+    ctx = myGameArea.context;
+    ctx.fillStyle = color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+  this.newPos = function() {
+    this.x = this.speedX;
+    this.y = this.speedY;
+  }
 }
 
-var myGameArea = {
-  canvas : document.createElement("canvas")
-  start : function() {
-    this.canvas.width = 480;
-    this.canvas.height = 270;
-    this.context = this.canvas.getContext("2D");
-    document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+function updateGameArea() {
+  myGameArea.clear();
+  myGamePiece.newPos();
+  if (myGameArea.key == 65) {
+    myGamePiece.speedX -= 1;
   }
+  if (myGameArea.key == 68) {
+    myGamePiece.speedX += 1;
+  }
+  if (myGameArea.key == 87) {
+    myGamePiece.speedY -= 1;
+  }
+  if (myGameArea.key == 83) {
+    myGamePiece.speedY += 1;
+  }
+  myGamePiece.update();
 }
